486f0bea1379b965eaf5b11796301f2c00cabfa0
sagarapidev <sagar.api.dev@gmail.com>
2026-03-11 00:39:21 +0530

    initial commit

---
+++ src/main/java/com/codex/eventhub_api_service/EventhubApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codex.eventhub_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubApiServiceApplication.class, args);
+	}
+
+}
